6287b5d929b7f149713dc914a9b5b19e4088255c
geekthattweaks <geekthattweaks@gmail.com>
2014-06-09 00:32:40 -0500

    Update udpSend.py

--- udpSend.py
+++ udpSend.py
@@ -7,3 +7,8 @@ MESSAGE = "Hello, World!"
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
+
+#What does this do?
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
